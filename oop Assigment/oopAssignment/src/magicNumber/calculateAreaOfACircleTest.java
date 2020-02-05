d711c5ec2ef837ae8a8a92662b7129a40082da31
pawan-98 <chhabriapawan01@gmail.com>
2020-02-05 18:09:00 +0530

    Program based on magic number is added

---
+++ oop Assigment/oopAssignment/src/magicNumber/calculateAreaOfACircleTest.java
@@ -0,0 +1,16 @@
+package magicNumber;
+
+public class calculateAreaOfACircleTest {
+
+	public static void main(String args[]) {
+		calculate(7);
+	}
+
+	public static void calculate(int radius) {
+		// defining value of pi as final because it is never going to change
+		final double PI = 3.14;
+		System.out.println(PI * radius * radius);
+
+	}
+
+}
