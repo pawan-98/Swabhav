509f63f53c9c77658e239e6ec8486afa25bed183
pawan-98 <chhabriapawan01@gmail.com>
2020-03-28 16:40:56 +0530

    account app added

---
+++ oop/account-app/src/com/techlab/model/Account.java
@@ -0,0 +1,36 @@
+package com.techlab.model;
+
+public class Account {
+
+	private double balance = 5000;
+	private static int MINIMUM_BALANCE;
+	private static int noOfTransactions = 0;
+
+	public Account(double balance) {
+		this.balance = balance;
+
+	}
+
+	public void withdraw(double amount) {
+		if (balance > MINIMUM_BALANCE) {
+			balance = balance - amount;
+			System.out.println("Current balance after withdrawing :- " + balance);
+			noOfTransactions=noOfTransactions+1;
+		}
+	}
+
+	public void deposit(double amount) {
+
+		balance = balance + amount;
+		System.out.println("Current balance after depositing :- " + balance);
+		noOfTransactions=noOfTransactions+1;
+	}
+	public static void setMinimumBalance(int minBlanace) {
+		MINIMUM_BALANCE = minBlanace;
+	}
+	public static int getNoOfTransaction() {
+		return noOfTransactions;	
+		}
+	
+
+}
---
+++ oop/account-app/src/com/techlab/model/test/AccountTest.java
@@ -0,0 +1,20 @@
+package com.techlab.model.test;
+
+import com.techlab.model.Account;
+
+public class AccountTest {
+
+	public static void main(String args[]) {
+		Account.setMinimumBalance(500);
+		
+		Account a1 =new Account(5000);
+		a1.withdraw(500);
+		a1.deposit(4000);
+		
+		Account a2 = new Account(7000);
+		a2.withdraw(700);
+		a2.deposit(600);
+		
+		System.out.println("Number of trnasactions:- "+ Account.getNoOfTransaction());
+	}
+}
