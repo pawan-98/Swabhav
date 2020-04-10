71f849b3b028909d781a295e43f6b567d5cd04bf
pawan-98 <chhabriapawan01@gmail.com>
2020-04-10 10:49:15 +0530

    week 10 assignment(edited the previous code)

--- oop Assigment/week10-property/src/com/techlab/csv/CsvTest.java
+++ oop Assigment/week10-property/src/com/techlab/csv/CsvTest.java
@@ -9,16 +9,35 @@ import java.util.Scanner;
 public class CsvTest {
 	public static void main(String args[]) throws FileNotFoundException {
 
-	String filename = "products_updated.csv";
-			File file = new File(filename);
-			
-			Scanner inputStream = new Scanner(file);
-			while(inputStream.hasNext()) {
-				String data = inputStream.next();
-				System.out.println(data+ "***" );
-				
+		String filename = "products_updated.csv";
+		File file = new File(filename);
+		String[] testArray = new String[10];
+
+		int i = 0;
+		Scanner inputStream = new Scanner(file);
+		while (inputStream.hasNext()) {
+
+			String data = inputStream.nextLine();
+
+			String[] products = data.split(",");
+
+			if (products.length == 4 && i != 0) {
+				displyPurchaseCost(products);
 			}
 
-			inputStream.close();
+			testArray[i] = data;
+			i++;
+		}
+
+		inputStream.close();
 	}
+
+	public static void displyPurchaseCost(String[] products) {
+		double finalAmount;
+		System.out.println("id= " + products[0] + " productName= " + products[1] + " Buying amount= " + products[2]
+				+ " Discount Percentage= " + products[3]);
+		finalAmount = (100 - Double.parseDouble(products[3])) * (Double.parseDouble(products[2])) / 100;
+		System.out.println("final amount after discount " + finalAmount);
+	}
+
 }
