ac8e917b7f6e9b1a03fd2f45968ce6ffc86c0ae4
pawan-98 <chhabriapawan01@gmail.com>
2020-11-16 18:25:38 +0530

    hibernate

--- Hibernate/manytomany-app/src/main/java/com/techlab/test/Test.java
+++ Hibernate/manytomany-app/src/main/java/com/techlab/test/Test.java
@@ -16,51 +16,51 @@ import com.techlab.entity.Movie;
 
 public class Test {
 	public static void main(String args[]) {
-		Movie movieOne= new Movie();
-		movieOne.setId(1);
-		movieOne.setMovieName("Movie One");
-		
-		Movie movieTwo= new Movie();
-		movieOne.setId(2);
-		movieOne.setMovieName("Movie Two");
-		
-		Actor pawan = new Actor();
-		pawan.setId(1);
-		pawan.setActorName("Pawan");
-		
-		Actor karan = new Actor();
-		karan.setId(2);
-		karan.setActorName("Karan");
-		
-		Actor tanishq = new Actor();
-		tanishq.setActorName("Tanishq");
-		tanishq.setId(3);
-		
-		Set<Actor> movieOneActors= new HashSet<Actor>();
-		movieOneActors.add(pawan);
-		movieOneActors.add(karan);
-		
-		Set<Actor> movieTwoActors= new HashSet<Actor>();
-		movieTwoActors.add(pawan);
-		movieTwoActors.add(tanishq);
-		
-		movieOne.setActors(movieOneActors);
-		movieTwo.setActors(movieTwoActors);
-		
-		Set<Movie> pawanMovies= new HashSet<Movie>();
-		pawanMovies.add(movieOne);
-		pawanMovies.add(movieTwo);
-		
-		Set<Movie> karanMovies = new HashSet<>();
-		karanMovies.add(movieOne);
-		
-		Set<Movie> tanishqMovies= new HashSet<Movie>();
-		tanishqMovies.add(movieTwo);
-		
-		pawan.setMovies(pawanMovies);
-		karan.setMovies(karanMovies);
-		tanishq.setMovies(tanishqMovies);
-		
+//		Movie movieOne= new Movie();
+//		movieOne.setId(1);
+//		movieOne.setMovieName("Movie One");
+//		
+//		Movie movieTwo= new Movie();
+//		movieTwo.setId(2);
+//		movieTwo.setMovieName("Movie Two");
+//		
+//		Actor pawan = new Actor();
+//		pawan.setId(1);
+//		pawan.setActorName("Pawan");
+//		
+//		Actor karan = new Actor();
+//		karan.setId(2);
+//		karan.setActorName("Karan");
+//		
+//		Actor tanishq = new Actor();
+//		tanishq.setActorName("Tanishq");
+//		tanishq.setId(3);
+//		
+//		Set<Actor> movieOneActors= new HashSet<Actor>();
+//		movieOneActors.add(pawan);
+//		movieOneActors.add(karan);
+//		
+//		Set<Actor> movieTwoActors= new HashSet<Actor>();
+//		movieTwoActors.add(pawan);
+//		movieTwoActors.add(tanishq);
+//		
+//		movieOne.setActors(movieOneActors);
+//		movieTwo.setActors(movieTwoActors);
+//		
+//		Set<Movie> pawanMovies= new HashSet<Movie>();
+//		pawanMovies.add(movieOne);
+//		pawanMovies.add(movieTwo);
+//		
+//		Set<Movie> karanMovies = new HashSet<>();
+//		karanMovies.add(movieOne);
+//		
+//		Set<Movie> tanishqMovies= new HashSet<Movie>();
+//		tanishqMovies.add(movieTwo);
+//		
+//		pawan.setMovies(pawanMovies);
+//		karan.setMovies(karanMovies);
+//		tanishq.setMovies(tanishqMovies);
+//		
 		Configuration cfg = new Configuration();
 		SessionFactory sessionFactory = cfg.configure("hibernate.cfg.xml").buildSessionFactory();
 		Session session= sessionFactory.openSession();
@@ -72,15 +72,15 @@ public class Test {
 			List<Movie> movies = new ArrayList<Movie>();
 			
 			transaction = session.beginTransaction();
-			session.save(pawan);
-			session.save(karan);
-			session.save(tanishq);
-			session.save(movieOne);
-			session.save(movieTwo);
-//			actors = session.createQuery("from Actor").list();
-//			movies= session.createQuery("from Movie").list();
-//			printData(actors);
-//			printData(movies);
+//			session.save(pawan);
+//			session.save(karan);
+//			session.save(tanishq);
+//			session.save(movieOne);
+//			session.save(movieTwo);
+			actors = session.createQuery("from Actor").list();
+			movies= session.createQuery("from Movie").list();
+			printData(actors);
+			printData(movies);
 			transaction.commit();
 			System.out.println("commit");
 		} catch (HibernateException ex) {
